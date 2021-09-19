91f1f054901159e08ac7deae516e92b16d297fea
Karim Metawea <ks.metawea@mohre.gov.ae>
2021-09-19 09:40:37 +0400

    Initial Commit

---
+++ FlagsApp/FlagsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsAppApp.swift
+//  FlagsApp
+//
+//  Created by Karem on 9/19/21.
+//
+
+import SwiftUI
+
+@main
+struct FlagsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
